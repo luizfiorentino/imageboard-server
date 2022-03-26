d56647fd01e3a6f3fcb4756a7053609f6371ba53
luizfiorentino <lgfiorentino@gmail.com>
2022-03-26 19:16:47 +0100

    Login endpoin finalized and working good

--- routers/auth.js
+++ routers/auth.js
@@ -1,21 +1,39 @@
 const { Router } = require("express");
 const { toJWT, toData } = require("../auth/jwt");
 const bcrypt = require("bcrypt");
+const res = require("express/lib/response");
 const User = require("../models").user;
 const Image = require("../models").image;
 
 const router = new Router();
 
 router.post("/login", async (req, res, next) => {
-  try {
-    const { email, password } = req.body;
-    if (!email && !password) {
-      res.status(400).send("Please enter a valid email and/or password");
-    } else {
-      res.send({ jwt: toJWT({ userId: 1 }) });
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    res
+      .status(400)
+      .send({ message: "Please enter a valid email and password" });
+  } else {
+    // * find user based on email address
+    const user = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
+    if (!user) {
+      res.status(400).send({ message: "This email is not found" });
     }
-  } catch (e) {
-    next(e);
+    // * use bcrypt.compareSync to check the received password against the stored hash
+    else if (bcrypt.compareSync(password, user.password)) {
+      // * if the password is correct, return a JWT with the userId of the user (user.id).
+      const jwt = toJWT({ userId: user.id });
+      res.send({
+        jwt,
+      });
+    } else {
+      res.status(400).send({ message: "Incorrect password" });
+    }
   }
 });
 
